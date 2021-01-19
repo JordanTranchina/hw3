303b9cd1bcff686ec605b80de29f1bfcaacb987a
Jordan Tranchina <Jordan12522@gmail.com>
2021-01-19 16:52:31 -0600

    Formatting the JS file

--- noober.js
+++ noober.js
@@ -2,8 +2,7 @@ function pageLoad() {
   // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
 
   // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
-  // the 'ride' variable contains data about a random ride (i.e. when the page)
-  // is refreshed or reloaded, the 'ride' variable will contain something different
+  // the 'ride' variable contains data about a random ride (i.e. when the page is refreshed or reloaded, the 'ride' variable will contain something different)
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
   console.log(ride)
@@ -11,16 +10,43 @@ function pageLoad() {
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
-  // levelOfService
-  // passenger1Name, passenger1Phone, passenger1NumberOfPassengers
-  // passenger1PickupAddressLine1, passenger1PickupAddressLine2
-  // passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
-  // passenger2Name, passenger2Phone, passenger2NumberOfPassengers
-  // passenger2PickupAddressLine1, passenger2PickupAddressLine2
-  // passenger2DropoffAddressLine1, passenger2DropoffAddressLine2
-  // passenger3Name, passenger3Phone, passenger3NumberOfPassengers
-  // passenger3PickupAddressLine1, passenger3PickupAddressLine2
-  // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
+  levelOfService = "Tacos"
+
+  // Passenger 1 - Overall
+  passenger1Name
+  passenger1Phone
+  passenger1NumberOfPassengers
+
+  // Passenger 1 - Pickup & Dropoff
+  passenger1PickupAddressLine1
+  passenger1PickupAddressLine2
+  passenger1DropoffAddressLine1
+  passenger1DropoffAddressLine2
+
+
+  // Passenger 2 - Overall
+  passenger2Name
+  passenger2Phone
+  passenger2NumberOfPassengers
+
+  // Passenger 2 - Pickup & Dropoff
+  passenger2PickupAddressLine1
+  passenger2PickupAddressLine2
+  passenger2DropoffAddressLine1
+  passenger2DropoffAddressLine2
+
+
+  // Passenger 3 - Overall
+  passenger3Name
+  passenger3Phone
+  passenger3NumberOfPassengers
+
+  // Passenger 2 - Pickup & Dropoff
+  passenger3PickupAddressLine1
+  passenger3PickupAddressLine2
+  passenger3DropoffAddressLine1
+  passenger3DropoffAddressLine2
+
 
   //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 }
