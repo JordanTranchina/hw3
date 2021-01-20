5a54727e535ab9a68214c51061c9ab6af50c52a9
Jordan Tranchina <Jordan12522@gmail.com>
2021-01-19 21:13:20 -0600

    first succesful implementation. Now checking requirements and refining

---
+++ reference.js
@@ -0,0 +1,27 @@
+const rideData =
+    [ //entire dataset
+        [ // group of riders
+            { //specific rider
+                "passengerDetails": {
+                    "first": "Thad",
+                    "last": "Farrell",
+                    "phoneNumber": "(989) 614-4016"
+                },
+                "pickupLocation": {
+                    "address": "551 W Elm St",
+                    "city": "Chicago",
+                    "state": "IL",
+                    "zip": "60612"
+                },
+                "dropoffLocation": {
+                    "address": "2550 S Wabash Ave",
+                    "city": "Chicago",
+                    "state": "IL",
+                    "zip": "60614"
+                },
+                "numberOfPassengers": 1,
+                "purpleRequested": false
+            }
+        ]
+    ]
+
